d4411cdea54970a3e9f5020dc78b11fc19cec507
vagrant <vagrant@localhost.localdomain>
2016-03-14 16:12:49 +0000

    avance2

--- practica2.php
+++ practica2.php
@@ -47,5 +47,72 @@ $trimmed = trim($, "HdWr");
 echo $trimmed;
 echo " \ņ";
 
+//substr
+
+
+echo substr("abcdef",1); //bcdef
+echo "/n";
+echo substr("abcdef",1, 3); //bcd
+echo "/n";
+echo substr("abcdef",0, 4); //abcd
+echo "/n";
+echo substr("abcdef",0, 8); //abcdef
+echo "/n";
+echo substr("abcdef",1, 1); //f
+
+
+$x = 0;
+$y = $X;
+$z = &x;
+var_dump($y);
+var_dump($z);
+$x = 10;
+var_dump($z);
+
+// Arreglos
+$associativo = array('Uno'=>1, 'Dos' =>2,'Dos' =>3,);
+// php ~> 5.3
+$associativo = [Uno'=>1,
+
+echo $associativo['Dos'];
+echo "\n";
+$associativo = [Uno'=>1,
+
+
+
+$array = array('Uno','Dos','Tres');
+echo $array[2];
+echo #\n";
+$array[3]='Cuatro';
+echo $array[3];
+echo #\n";
+
+array_push(array, 'Cinco');
+var_dump($array);
+
+//Lgica
+
+
+
+
+
+assert($a == $b);
+//assert($a===$b); //esto es falso
+assert($c !=$a);//true
+assert($c<> $a);//true
+assert($a < $c);//true
+assert($c < $b);//true
+assert($a <= $b);//true
+assert($c >= $d);//true
+
+assert($c === $d);
+assert($a !== $d);
+assert
+assert
+
+
+
+
+
 
 
